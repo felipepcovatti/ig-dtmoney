63f9ca63b333e0b7ceb1e05cdfef44c56943cf92
Felipe P Covatti <felipepcovatti@gmail.com>
2021-03-17 14:30:27 -0300

    Fix css font unit

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -19,7 +19,7 @@ export const GlobalStyle = createGlobalStyle`
 
   html {
     @media(max-width: 1080px) {
-      font-size: 93.75px;
+      font-size: 93.75%;
     }
 
     @media(max-width: 720px) {
