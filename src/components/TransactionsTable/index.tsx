4b0d07a586d4431b796a805354308873b34ab6d1
Felipe P Covatti <felipepcovatti@gmail.com>
2021-03-22 16:13:23 -0300

    Add fake api and modal that creates transactions

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,8 +1,23 @@
+import { useEffect, useState } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
-export function TransactionsTable() {
+interface Transaction {
+  id: string
+  title: string
+  amount: number
+  type: 'deposit' | 'widtraw'
+  category: string
+  createdAt: string;
+}
 
-  const transactions = [1, 2, 3]
+export function TransactionsTable() {
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+  useEffect(() => {
+    api.get('transactions').then(response => {
+      setTransactions(response.data.transactions)
+    })
+  }, [])
 
   return (
     <Container>
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import { createServer, Model } from 'miragejs'
+
+createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          amount: 234,
+          category: 'Desenvolvimento',
+          type: 'deposit',
+          createdAt: new Date('2021-02-12 09:00:00')
+        },
+        {
+          id: 2,
+          title: 'Pizza',
+          amount: 23,
+          category: 'Comida',
+          type: 'withdraw',
+          createdAt: new Date('2021-02-15 12:00:00')
+        },
+      ]
+    })
+  },
+
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return this.schema.all('transaction')
+    });
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transaction', data)
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
